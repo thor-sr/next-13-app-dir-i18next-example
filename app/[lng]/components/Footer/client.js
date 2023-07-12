ab2c05fdba04383a54efe7f7d07cdbb79d221237
thor-sr <thorsteinn@seoulrobotics.org>
2023-07-12 17:05:38 +0900

    Remove all unnecessary files

--- app/[lng]/components/Footer/client.js
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { FooterBase } from './FooterBase'
-import { useTranslation } from '../../../i18n/client'
-
-export const Footer = ({ lng, path }) => {
-  const { t } = useTranslation(lng, 'footer')
-  return <FooterBase t={t} lng={lng} path={path} />
-}
